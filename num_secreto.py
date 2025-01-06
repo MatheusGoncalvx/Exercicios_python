b3e41a24a44dd1325d42c6383da38e077a327c84
MatheusGoncalvx <matheus.lima419@etec.sp.gov.br>
2025-01-06 01:44:13 -0300

    update

--- num_secreto.py
+++ num_secreto.py
@@ -1,8 +1,8 @@
 ## Declaração de Variavéis
 user = int(input("Digite um número: "))
-
 num = 7
 
+## Verificando se o número for diferente do que o user digitar
 while user != num:
     if user < num:
         print(" Tente um número maior ")
@@ -12,4 +12,4 @@ while user != num:
     user = int(input("Digite outro numéro: "))
 
     if user == num:
-        print("Parabéns você acertou o número secreto!!!!!!!!!")
+        print("Parabéns você acertou o número secreto!!!")
